ea42fe4550b42bc9448afce367afa7978ea8a2f4
rivanildo <Rivanildo@MacBook-Pro-de-Rivanildo.local>
2016-03-11 18:59:05 -0300

    Exemplo 2, giro de 180º

---
+++ exemplo2/exemplo2.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+ 
+Servo motor;
+void setup() { 
+ motor.attach(3);
+} 
+
+void loop() { 
+ int i;
+  for(i=0;i<180;i++) {
+    motor.write(i); 
+    delay(10); 
+ }
+  delay(1000);
+  for(i=180;i>0;i--) {
+    motor.write(i); 
+    delay(10); 
+ }
+ delay(1000);
+} 
